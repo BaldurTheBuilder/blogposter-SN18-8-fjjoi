38f9d561e6d6ed8928746628b666d75b18bc180c
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-04-04 15:21:07 -0700

    signup back end complete

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,45 +1,68 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
 // logout post
-router.post('/logout', (req, res) => {
-    if(req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else res.status(400).end();
-})
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else res.status(400).end();
+});
 
 // login post
-router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.findOne({
-            where: {
-                username: req.body.username
-            }
-        });
-        // check if a user exists
-        if(!userData) {
-            res.status(400);
-            return;
-        }
-        const passwordCheck = userData.checkPassword(req.body.password);
-        // check the password is valid
-        if(!passwordCheck) {
-            res.status(400);
-            return;
-        }
-        // if valid, we save logged_in and user_id to session
-        req.session.save(() => {
-            req.session.logged_in = true;
-            req.session.user_id = userData.id;
-            res.json({user: userData, message: "Logged in"});
-        });
-    } catch (err) {
-        res.status(500).json(err);
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    // check if a user exists
+    if (!userData) {
+      res.status(400);
+      return;
     }
+    const passwordCheck = userData.checkPassword(req.body.password);
+    // check the password is valid
+    if (!passwordCheck) {
+      res.status(400);
+      return;
+    }
+    // if valid, we save logged_in and user_id to session
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = userData.id;
+      res.json({ user: userData, message: "Logged in" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //sign up post
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    // check if a username already exists
+    if (userData) {
+      res.status(400);
+      return;
+    }
+    User.create(req.body).then((newUser) => {
+      req.session.save(() => {
+        req.session.logged_in = true;
+        req.session.user_id = newUser.id;
+        res.json({ user: newUser, message: "User created and logged in" });
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
