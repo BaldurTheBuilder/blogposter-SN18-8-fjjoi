d1e431a2508c721aa83d1ce6546aa9422717482b
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-04-04 15:24:03 -0700

    unknown network error when trying to post

--- public/js/login.js
+++ public/js/login.js
@@ -23,6 +23,23 @@ $(() => {
 
   const signup = async (event) => {
     event.preventDefault();
+    const username = $("#signup-username").val().trim();
+    const password = $("#signup-password").val().trim();
+    if(!username || !password) {
+        alert('please complete the form.');
+        return;
+    }
+
+    const response = await fetch("/api/users/signup", {
+        method: "POST",
+        body: `{"username": "${username}", "password": "${password}"}`,
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        console.log('user created and logged in!');
+        document.location = "/";
+      } else alert("error signing up.");
   };
 
   $("#login-form").submit(login);
