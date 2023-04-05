996293fcf37ea9e744feea637820a2c97ad7992a
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-04-04 17:30:51 -0700

    front/back integration error

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { BlogPost } = require("../../models");
+const { BlogPost, Comment } = require("../../models");
 
 // create blogpost
 router.post("/", async (req, res) => {
@@ -18,6 +18,27 @@ router.post("/", async (req, res) => {
 });
 
 // delete blogpost
+router.delete('/:id', async (req, res) => {
+    try {
+      const postData = await BlogPost.findByPk(req.params.id);
+      if(!postData) {
+        res.status(404).json({message: 'Unable to delete the requested blogpost.'});
+        return;
+      }
+      
+      const serializedPost = postData.get({plain: true});
+      if(serializedPost.user_id !== req.session.user_id) {
+        res.status(401).json({message: "Unable to delete the requested blogpost."});
+        return;
+      }
+      const destroyPost = await BlogPost.destroy({
+        where: {id: req.params.id}
+      });
+      res.status(200).json(destroyPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 // update blogpost
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,6 +21,22 @@ $(() => {
     } else alert("error creating a post.");
   };
 
+  const deletePost = async (event) => {
+    event.preventDefault();
+    const blogPost_id = $(this).data("blogPost_id");
+    const response = await fetch(`/api/blogposts/${blogPost_id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Failed to delete the blogpost.");
+    }
+  }
+
+
   const revealNewPost = () => {
     $("#hidden-div").removeClass("visually-hidden");
   };
@@ -30,6 +46,7 @@ $(() => {
   };
 
   $("#new-post-form").submit(createPost);
+  $("#button[id^='del-post-btn']").click(deletePost);
   $("#reveal-new-post-btn").click(revealNewPost);
   $("div[id^='individual-post']").click(revealExistingPost);
 });
