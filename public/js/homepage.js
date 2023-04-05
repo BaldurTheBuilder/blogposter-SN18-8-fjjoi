31b2291586ffa026064424a09db425358448036a
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-04-05 02:45:47 -0700

    front end looks good

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,8 +1,50 @@
 $(() => {
-    function revealExistingPost () {
-        $(this).find('*').removeClass("visually-hidden");
-      };
+  async function createComment(event) {
+    event.preventDefault();
+    const commentContents = $(this).children(1).val().trim();
+    const blogPost_id = $(this).data("blog_id");
+    if (!commentContents) {
+      alert("please compose a comment.");
+      return;
+    }
 
-      
-    $("div[id^='post']").click(revealExistingPost);
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: `{"contents": "${postContents}", "blogPost_id": "${blogPost_id}"}`,
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      console.log("Post created.");
+      location.reload();
+    } else alert("error creating a post.");
+  }
+
+  function revealExistingPost() {
+    $(this).find("*").removeClass("visually-hidden");
+  }
+
+  $("div[id^='post']").click(revealExistingPost);
+  $("form[id^='comment-form']").submit(createComment);
 });
+
+// <form id="comment-form-{{blogPost.id}}">
+//     <p>Leave a Comment:</p>
+//     <textarea id="comment-contents-{{blogPost.id}}" data-id="" placeholder="comment"></textarea>
+//     <button type="submit" id="comment-submit-btn-{{blogPost.id}}">Submit Comment</button>
+// </form>
+
+// user_id: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: 'user',
+//       key: 'id',
+//     },
+//   },
+// blogPost_id: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: 'blogPost',
+//       key: 'id',
+//     },
+// }
